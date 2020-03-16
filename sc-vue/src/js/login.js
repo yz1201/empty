999a0aa46615327563e8bdab3aa4b5bd46e791af
yz1201 <15957121194>
2020-03-17 02:05:54 +0800

    shopping city -st vue

---
+++ sc-vue/src/js/login.js
@@ -0,0 +1,20 @@
+const loginForm = {
+    template: `
+        <div>
+            <h1>Login Page</h1>
+            username: <input type="text"><br/>
+            password:<input type="text"><br/>
+            <button type="button">Login</button>
+        </div>
+    `
+};
+
+// const loginForm = {
+//     template:'\
+//     <div>\
+//     <h2>登录页</h2> \
+//     用户名：<input type="text"><br/>\
+//     密码：<input type="password"><br/>\
+//     </div>\
+//     '
+// };
---
+++ sc-vue/src/js/register.js
@@ -0,0 +1,21 @@
+const registerForm = {
+    template: `
+        <div>
+            <h1>Register Page</h1>
+            username: <input type="text"><br/>
+            password:<input type="text"><br/>
+            confirm password:<input type="text"><br/>
+            <button type="button">Register</button><br/>
+        </div>
+    `
+};
+// const registerForm = {
+//     template:'\
+//     <div>\
+//     <h2>注册页</h2> \
+//     用&ensp;户&ensp;名：<input type="text"><br/>\
+//     密&emsp;&emsp;码：<input type="password"><br/>\
+//     确认密码：<input type="password"><br/>\
+//     </div>\
+//     '
+// };
