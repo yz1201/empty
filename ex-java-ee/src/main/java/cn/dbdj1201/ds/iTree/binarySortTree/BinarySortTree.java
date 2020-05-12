1fe3c7f061f83d3bc5b49f3bf8a807c8b1f6954f
yz1201 <15957121194>
2020-05-12 11:46:35 +0800

    ds -tree  BinarySortTree 2nd

--- ex-java-ee/src/main/java/cn/dbdj1201/ds/iTree/binarySortTree/BinarySortTree.java
+++ ex-java-ee/src/main/java/cn/dbdj1201/ds/iTree/binarySortTree/BinarySortTree.java
@@ -40,9 +40,119 @@ public class BinarySortTree {
     /*
     二叉排序树的删除，分三种情况：
     1，删除叶子节点
+        直接删除，找到待删除叶子节点的父节点，判断是左子节点还是右子节点，将parent.left =null或者parent.right = null
     2，删除只有一颗子树的节点
+        先找到待删除节点targetNode
+        找到其父节点parent
+        确定targetNode的子节点是左子节点还是右子节点
+        targetNode是parent的左子节点还是右子节点
+        若target的子节点是左子节点
+            则为parent.left = targetNode.left;        //是parent的左子节点
+                或者parent.right = targetNode.left.//是parent的右子节点
+        若是右
+            parent.left = targetNode.right 或者 parent.right = targetNode.right
     3，删除有两棵子树的节点
+        找到targetNode及parent
+        从targetNode的右子树找到最小节点
+        用临时变量保存最小节点
+        删除该节点
+        targetNode = temp  //要保证是二叉排序树BST，所有从右边找，且找最小。||或者从左子树找一个最大的，一样的道理。
      */
+
+    public Node3 search(int value) {
+        if (root == null)
+            return null;
+        else
+            return root.search(value);
+    }
+
+    public Node3 searchParent(int value) {
+        if (root == null)
+            return null;
+        else
+            return root.searchParent(value);
+    }
+
+    /**
+     * @param node3
+     * @return 删除最小节点，返回最小节点的值
+     */
+    public int delRightTreeMin(Node3 node3) {
+        Node3 target = node3;
+        while (target.left != null) {
+            target = target.left;
+        }
+        delNode(target.value);//删除权值最小的节点
+        return target.value;
+    }
+
+    public void delNode(int value) {
+        if (root == null)
+            return;
+        else {
+            Node3 targetNode = search(value);
+            if (targetNode == null)
+                return;
+            //如果当前二叉树只有一个root节点,且是我们要删除的节点
+            if (root.left == null && root.right == null) {
+                root = null;
+                return;
+            }
+
+            Node3 parent = searchParent(value);
+            //删除叶子节点
+            if (targetNode.left == null && targetNode.right == null) {
+                //是左子节点
+                if (parent.left != null && parent.left.value == value) {
+                    parent.left = null;
+
+                } else if (parent.right != null && parent.right.value == value) {
+                    //删除右子节点
+                    parent.right = null;
+                }
+            } else if (targetNode.left != null && targetNode.right != null) {
+                //删除儿女都有的节点
+                /*
+                删除有两棵子树的节点
+                找到targetNode及parent
+                从targetNode的右子树找到最小节点
+                用临时变量保存最小节点
+                删除该节点
+                targetNode = temp  //要保证是二叉排序树BST，所有从右边找，且找最小。
+                 */
+                targetNode.value = delRightTreeMin(targetNode.right);
+//                targetNode.value = delLeftTreeMax(targetNode.left);
+            } else {
+                //删除只有一颗子树的节点
+                //判断target有左还是右子节点
+                if (targetNode.left != null) {
+                    //判断target是parent的左子节点还是右子节点
+                    if (parent.left.value == value) {
+                        parent.left = targetNode.left;
+                    } else {
+                        parent.right = targetNode.right;
+                    }
+                } else {
+                    if (parent.left.value == value)
+                        parent.left = targetNode.right;
+                    else
+                        parent.right = targetNode.right;
+                }
+            }
+
+
+        }
+    }
+
+    //删除左子树最大节点，即把左子树中最大节点替换到当前位置
+    private int delLeftTreeMax(Node3 left) {
+        Node3 target = left;
+        while (target != null) {
+            target = target.right;
+        }
+        delNode(target.value);
+        return target.value;
+    }
 
 }
 
@@ -88,6 +198,7 @@ class Node3 {
         }
     }
 
+    //中序遍历
     public void infixOrder() {
         if (this.left != null)
             this.left.infixOrder();
@@ -97,4 +208,39 @@ class Node3 {
         if (this.right != null)
             this.right.infixOrder();
     }
+
+    //查找待删除节点
+    public Node3 search(int value) {
+        if (value == this.value) {
+            return this;
+        } else if (value < this.value) {
+            if (this.left != null)
+                return this.left.search(value);
+            else
+                return null;
+        } else {
+            if (this.right == null)
+                return null;
+            return this.right.search(value);
+        }
+    }
+
+    //查找待删除节点的父节点
+    public Node3 searchParent(int value) {
+        if ((this.left != null && this.left.value == value) || (this.right != null && this.right.value == value)) {
+            return this;
+        } else {
+            //向左查
+            if (value < this.value && this.left != null) {
+                return this.left.searchParent(value);
+            } else if (value >= this.value && this.right != null) {
+                //向右查
+                return this.right.searchParent(value);
+            } else {
+                return null;
+            }
+        }
+
+    }
+
 }
