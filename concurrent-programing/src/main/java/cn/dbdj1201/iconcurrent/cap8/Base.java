0f09927262199cc914faf1c7e1e77ac37d3f5995
yz1201 <15957121194>
2020-08-01 18:45:55 +0800

    concurrent -st cpt6 原子类

---
+++ concurrent-programing/src/main/java/cn/dbdj1201/iconcurrent/cap8/Base.java
@@ -0,0 +1,8 @@
+package cn.dbdj1201.iconcurrent.cap8;
+
+/**
+ * @author tyz1201
+ * @datetime 2020-06-25 14:31
+ **/
+public class Base {
+}
---
+++ concurrent-programing/src/main/java/cn/dbdj1201/iconcurrent/cap8/Test062501.java
@@ -0,0 +1,15 @@
+package cn.dbdj1201.iconcurrent.cap8;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author tyz1201
+ * @datetime 2020-06-25 14:32
+ **/
+public class Test062501 {
+    public static void main(String[] args) {
+        ExecutorService service;
+        ThreadPoolExecutor executor;
+    }
+}
